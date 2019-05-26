9ab6da300be86d2c2613842ce36dcce5c6bb939f
vikatkach <50865377+vikatkach@users.noreply.github.com>
2019-05-26 15:28:39 +0300

    change encoding

--- LizaLab2/Header.h
+++ LizaLab2/Header.h
@@ -11,25 +11,25 @@
 
 using namespace std;
 
-class IPoly {             //ęëŕńń ĚÍÎĂÎ×ËĹÍ.
+class IPoly {             //класс МНОГОЧЛЕН
 public:
 	ui n;
 	vector <ui> p;
 
-	IPoly();				//ęîíńňđóęňîđ
-	IPoly(vector <ui>);		//ęîíńňđóęňîđ
+	IPoly();				//конструктор
+	IPoly(vector <ui>);		//конструктор
 
-	IPoly add(IPoly);			//ěĺňîä +
-	IPoly multiply(IPoly);		//ěĺňîä *
-	IPoly divide(IPoly);		//ěĺňîä /
-	IPoly mod(IPoly);			//îńňŕ÷ŕ  îň äĺëĺíč˙
-	IPoly derivate();			//ďđîčçâîäíŕ˙
-	IPoly integrate();			//číňĺăđŕë íĺîďđĺäĺëĺííűé
+	IPoly add(IPoly);			//метод +
+	IPoly multiply(IPoly);		//метод *
+	IPoly divide(IPoly);		//метод /
+	IPoly mod(IPoly);			//остача  от деления
+	IPoly derivate();			//производная
+	IPoly integrate();			//интеграл неопределенный
 
-	void print(ostream &);		//âűâîä
-	ui integrate(ui, ui);		//îďđĺäĺëĺííűé číňĺăđŕë
-	ui value(ui);				//çíŕ÷ĺíčĺ â ňî÷ęĺ
-	IPoly operator= (IPoly);	//îďĺđŕňîđ äë˙ ďđčńâŕčâŕíč˙ 
+	void print(ostream &);		//вывод
+	ui integrate(ui, ui);		//определенный интеграл
+	ui value(ui);				//значение в точке
+	IPoly operator= (IPoly);	//оператор для присваивания 
 	
 };
 
